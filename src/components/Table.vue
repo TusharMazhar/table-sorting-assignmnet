4344c9ccb84349b437eed9ad7eea9a1676ef21f8
Tushar-Mazhar <tushar.jatri@gmail.com>
2022-02-21 12:10:58 +0600

    first commit

---
+++ src/components/Table.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+    <v-app>
+        <v-card width="50%" class="table">
+            <v-data-table
+                :headers="headers"
+                :items="sampleData"
+                :search="search"
+                hide-default-footer
+                disable-filtering
+            ></v-data-table>
+        </v-card>
+    </v-app>
+</div>
+</template>
+
+<script>
+
+export default ({
+    name:'Table',
+    data () {
+    return {
+      headers: [
+        { text: 'Book ID',value: 'Book_ID',},
+        { text: 'Book Name', value: 'Book_Name' },
+        { text: 'Category', value: 'Category' },
+        { text: 'Price', value: 'Price' },
+
+      ],
+      sampleData: [
+        {
+            'Book_ID': '1', 
+            'Book_Name': 'Challenging Times',
+            'Category': 'Business',
+            'Price': '125.60'
+        },
+        {
+            'Book_ID': '2', 
+            'Book_Name': 'Learning JavaScript',
+            'Category': 'Programming', 
+            'Price': '56.00'
+        },
+        {
+            'Book_ID': '3', 
+            'Book_Name': 'Popular Science',
+            'Category': 'Science', 
+            'Price': '210.40'
+        }
+
+   
+      ],
+    }
+  },
+  
+})
+</script>
+<style scoped>
+ .table{
+     margin: auto;
+ }
+</style>
+
